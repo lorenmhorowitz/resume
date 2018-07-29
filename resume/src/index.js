b7c61c99190f356e9355587732e39d6cf5aec323
lorenmhorowitz <lorenmhorowitz@gmail.com>
2018-07-29 14:09:04 -0700

    Installed Material-UI

--- resume/src/index.js
+++ resume/src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { AppContainer } from "react-hot-loader";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<AppContainer>
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+</AppContainer>, document.getElementById('root'));
 registerServiceWorker();
